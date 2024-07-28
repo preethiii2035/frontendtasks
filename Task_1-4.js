8c6d1e0fc8f16d7127e19f37d9511c2902b21c94
preethiii2035 <preethivini2004@gmail.com>
2024-07-28 22:09:28 +0530

    Add files via upload

---
+++ Task_1-4.js
@@ -0,0 +1,11 @@
+function changeColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  document.getElementById('colorButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = changeColor();
+  });
